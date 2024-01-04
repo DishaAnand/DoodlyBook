2a10c1a21205e7bb787973011eb7ec46a13af7cd
DISHA ANAND <ds442718@dal.ca>
2024-01-04 12:33:46 -0400

    feat:emitting events

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -4,6 +4,7 @@ import { useSelector , useDispatch } from 'react-redux'
 import { MENU_ITEMS } from "../../constants"
 import { menuItemClick,actionItemClick } from '../../slice/menuSlice';
 
+import { socket } from "../../socket"
 const Board = () => {
   const dispatch = useDispatch()
   const canvasRef = useRef(null)
@@ -39,12 +40,20 @@ const Board = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d')
 
-    const changeConfig =() =>{
+    const changeConfig =(color,size) =>{
       context.strokeStyle = color
       context.lineWidth = size
     }
 
-    changeConfig()
+    const handleChangeConfig =(config)=>{
+      changeConfig(config.color,config.size)
+    }
+    changeConfig(color,size)
+    socket.on('changeConfig',handleChangeConfig)
+
+    return()=>{
+      socket.off('changeConfig',handleChangeConfig)
+    }
   },[color,size])
 
   //mounting 
@@ -57,16 +66,28 @@ const Board = () => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
-    const handleMouseDown = (e) =>{
-      shouldDraw.current = true
+    const beginPath =(x,y) =>{
       context.beginPath()
-      //context.moveTo(e.clientX,e.clientY)
+      context.moveTo(x,y)
     }
-    const handleMouseMove =(e) =>{
-      if(!shouldDraw.current) return 
-      context.lineTo(e.clientX,e.clientY)
+
+    const drawLine = (x,y) => {
+      context.lineTo(x,y)
       context.stroke()
     }
+
+    const handleMouseDown = (e) =>{
+      shouldDraw.current = true
+      beginPath(e.clientX,e.clientY)
+      socket.emit('beginPath',{x:e.clientX,y:e.clientY})
+    }
+    
+    const handleMouseMove =(e) =>{
+      if(!shouldDraw.current) return 
+      drawLine(e.clientX,e.clientY)
+      socket.emit('drawLine',{x:e.clientX,y:e.clientY})
+    }
+
     const handleMouseUp = (e) =>{
       shouldDraw.current = false
       const imageData = context.getImageData(0,0, canvas.width, canvas.height)
@@ -75,14 +96,28 @@ const Board = () => {
        
     }
 
+    const handleBeginPath = (path) =>{
+      beginPath(path.x,path.y)
+    }
+
+    const handleDrawline = (path) =>{
+      drawLine(path.x,path.y)
+    }
+
     canvas.addEventListener('mousedown', handleMouseDown)
     canvas.addEventListener('mousemove',handleMouseMove)
     canvas.addEventListener('mouseup',handleMouseUp)
+
+    socket.on('beginPath',handleBeginPath)
+    socket.on('drawLine',handleDrawline)
 
     return () =>{
       canvas.removeEventListener('mousedown', handleMouseDown)
       canvas.removeEventListener('mousemove',handleMouseMove)
       canvas.removeEventListener('mouseup',handleMouseUp)
+
+      socket.off('beginPath',handleBeginPath)
+      socket.off('drawLine',handleDrawline)
     }
   },[])
   return (<canvas ref = {canvasRef}></canvas>)
--- src/components/Toolboxs/index.js
+++ src/components/Toolboxs/index.js
@@ -4,6 +4,7 @@ import {COLORS, MENU_ITEMS} from '../../constants'
 import { useSelector , useDispatch } from 'react-redux'
 // eslint-disable-next-line import/no-unresolved
 import { changeColor,changeBrushSize } from '@/slice/toolboxSlice'
+import { socket } from "../../socket"
 
 const Toolboxs = () =>{
   const dispatch = useDispatch()
@@ -14,10 +15,12 @@ const Toolboxs = () =>{
 
   const updateBrushSize = (e) =>{
     dispatch(changeBrushSize({item:activeMenuItem, size: e.target.value}))
+    socket.emit('changeConfig',{color,size:e.target.value})
   }
 
   const updateColor =(newColor) =>{
     dispatch(changeColor({item:activeMenuItem,color: newColor}))
+    socket.emit('changeConfig',{color:newColor,size})
   }
   return (<div className={styles.toolboxContainer}>
     {showStrokeToolOption &&  <div className={styles.toolItem}>
